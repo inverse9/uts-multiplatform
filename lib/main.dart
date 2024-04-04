c318735195c8978d848a2049fcb24d18e8039007
inverse9 <permanawahyu97@gmail.com>
2024-04-04 12:32:14 +0800

    apply theme & typhograpy

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          useMaterial3: true,
+          colorSchemeSeed: Colors.brown,
         ),
         home: Home(),
       ),
@@ -39,14 +40,23 @@ class _HomeState extends State<Home> {
             currentPageIndex = index;
           });
         },
-        indicatorColor: Colors.indigo,
+        indicatorColor: Theme.of(context).colorScheme.primary,
         selectedIndex: currentPageIndex,
         destinations: [
           NavigationDestination(
-              selectedIcon: Icon(Icons.home),
+              selectedIcon: Icon(
+                Icons.home,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
               icon: Icon(Icons.home_outlined),
-              label: "home"),
-          NavigationDestination(icon: Icon(Icons.favorite), label: "wishlist"),
+              label: "Home"),
+          NavigationDestination(
+              selectedIcon: Icon(
+                Icons.favorite,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              icon: Icon(Icons.favorite_border),
+              label: "Wishlist"),
         ],
       ),
       body: [GridPage(), WishlistPage()][currentPageIndex],
@@ -77,7 +87,9 @@ class GridPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Test'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text('Product',
+            style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
       ),
       body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -120,29 +132,67 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detail'),
+        iconTheme:
+            IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text('Detail',
+            style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
       ),
-      body: Column(
+      body: ListView(
         children: [
           Image.network(
             data["img"]!,
             fit: BoxFit.cover,
           ),
-          Text(data["desc"]!),
-          Consumer<WishlistNotifier>(
-            builder: (context, notifier, _) => IconButton(
-              icon: Icon(
-                notifier.wishlist.any((item) => item["id"] == data["id"])
-                    ? Icons.favorite
-                    : Icons.favorite_border,
-                color: notifier.wishlist.any((item) => item["id"] == data["id"])
-                    ? Colors.red
-                    : Colors.grey,
-              ),
-              onPressed: () {
-                notifier.toggleWishlist(data);
-              },
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Kipas angin portable full dingin",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize:
+                          Theme.of(context).textTheme.titleLarge?.fontSize,
+                      fontWeight: FontWeight.w700),
+                ),
+                Consumer<WishlistNotifier>(
+                  builder: (context, notifier, _) => IconButton(
+                    icon: Icon(
+                      notifier._wishlist.any((item) => item["id"] == data["id"])
+                          ? Icons.favorite
+                          : Icons.favorite_border,
+                      color: notifier._wishlist
+                              .any((item) => item["id"] == data["id"])
+                          ? Colors.red
+                          : Colors.grey,
+                    ),
+                    onPressed: () {
+                      notifier.toggleWishlist(data);
+                    },
+                  ),
+                ),
+              ],
             ),
+          ),
+          ListTile(
+            title: Text("Deskripsi",
+                style: Theme.of(context).textTheme.titleSmall),
+            subtitle: Text(
+              """Creates a text widget.If the [style] argument is null, 
+          the text will use the style from the closest enclosing 
+          The [overflow] property's behavior is affected by the [softWrap] argument.
+          If the [softWrap] is true or null, the glyph causing overflow, and those that follow,
+          will not be rendered. Otherwise, it will be shown with the given overflow option""",
+              style: TextStyle(color: Colors.grey, fontSize: 12),
+            ),
+          ),
+          ListTile(
+            title:
+                Text("Kondisi", style: Theme.of(context).textTheme.titleSmall),
+            subtitle: Text("Baru",
+                style: TextStyle(color: Colors.grey, fontSize: 12)),
           ),
         ],
       ),
@@ -151,9 +201,7 @@ class DetailPage extends StatelessWidget {
 }
 
 class WishlistNotifier extends ChangeNotifier {
-  Set<Map<String, String>> _wishlist = {};
-
-  Set<Map<String, String>> get wishlist => _wishlist;
+  final Set<Map<String, String>> _wishlist = {};
 
   void toggleWishlist(Map<String, String> data) {
     var id = data["id"];
@@ -161,7 +209,6 @@ class WishlistNotifier extends ChangeNotifier {
       _wishlist.removeWhere((item) => item["id"] == id);
     } else {
       _wishlist.add(data);
-      print(_wishlist);
     }
     notifyListeners();
   }
@@ -175,12 +222,12 @@ class WishlistPage extends StatelessWidget {
     var appState = context.watch<WishlistNotifier>();
     if (appState._wishlist.isEmpty) {
       return Center(
-        child: Text('No favorites yet.'),
+        child: Text('wishlist kosong'),
       );
     }
     return GridView.builder(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // Adjust the cross axis count as needed
+        crossAxisCount: 2,
         crossAxisSpacing: 4.0,
         mainAxisSpacing: 4.0,
       ),
